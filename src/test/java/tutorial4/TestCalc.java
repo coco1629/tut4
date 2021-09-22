794fe3a4975fc266a23197dbeacc4d7b8b1ebe79
coco <76069220@qq.com>
2021-09-22 23:38:50 +0800

    Add test substract

--- src/test/java/tutorial4/TestCalc.java
+++ src/test/java/tutorial4/TestCalc.java
@@ -12,4 +12,11 @@ public class TestCalc {
         int answer = calc.add(2,3);
         assertEquals(answer,5);
     }
+
+    @Test
+    public void TestSubstract(){
+        Calc calc = new Calc();
+        int answer = calc.substract(10, 1);
+        assertEquals(answer, 9);
+    }
 }
